7003d9f0b73000e6aace88a17bebadb9b166d2b7
Loskir <kl@sinker.ru>
2021-05-14 12:57:58 +0300

    Add router Support multiple boards

---
+++ src/views/Home.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import page from 'page'
+
+  let boardId = ''
+
+  const go = () => page.show(`/board/${boardId}`)
+</script>
+
+<main class="main">
+  <h1>Hi! Enter board id</h1>
+  <form on:submit|preventDefault={go}>
+    <input bind:value={boardId}>
+    <button type="submit">Go</button>
+  </form>
+</main>
+
+<style lang="stylus">
+  .main {
+    flex-grow 1
+    display flex
+    flex-direction column
+    justify-content center
+    align-items center
+  }
+</style>
